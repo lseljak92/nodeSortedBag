1c6bd2ecc91eee37d9fdfecd9b36d9b075c37db9
Luciana Seljak <35976629+lseljak92@users.noreply.github.com>
2018-10-04 05:44:15 -0700

    Add header

--- NodeBasedBagTest.java
+++ NodeBasedBagTest.java
@@ -1,17 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Homework 02
+ * BagInterface.java
+ * Devon Dy-Liacco, Christian Cortez, Maria Seljak
  */
 package nodebasedarray;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
-/**
- *
- * @author abeer
- */
+
 public class NodeBasedBagTest {
 
     /**
