4c8ef675bbcc8d34f0fa9e0cb0bb0e0fc3ef2bb1
Luciana Seljak <35976629+lseljak92@users.noreply.github.com>
2018-10-04 05:42:35 -0700

    Add header

--- Coin.java
+++ Coin.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Homework 02
+ * BagInterface.java
+ * Devon Dy-Liacco, Christian Cortez, Maria Seljak
  */
 package nodebasedarray;
 
-/**
- *
- * @author lucianaseljak
- */
 public class Coin implements Comparable<Coin> {
 
     private int coinValue;
